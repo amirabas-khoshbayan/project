32b2372e9f7db1b11685229a8b8b3b708706e7d4
AmirAbbas <amirabas.khoshbayan@gmail.com>
2021-05-09 00:40:31 +0430

    Edit main.go

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type person struct {
+	name string
+	family string
+	age int
+}
 
 func main()  {
 	fmt.Println("LOVE GOLANG")
+ example :=  person{
+  	name:  "AmirAbbas",
+  	family : "KhoshBayan",
+  	age : 21,
+  }
+   fmt.Println(example)
+   fmt.Printf("i am %s and Love golang language,my family is %s,The best age to learn %d",example.name,example.family,example.age)
+}
+
+func (p *person)  test(){
+	ptest := new(person)
+	ptest.name = "Hossain"
+	ptest.family = "KhoshBayan"
+	ptest.age = 25
 }
